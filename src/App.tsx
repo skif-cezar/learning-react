74d865346291059af8ba4e6c27d057dc2d3916e7
skif-cezar <skif.cezar@gmail.com>
2022-08-20 15:43:19 +0300

    feat: add loader

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,15 @@ import { IProduct } from "./models";
 
 function App() {
   const [products, setProducts] = useState<IProduct[]>([]);
+  const [loading, setLoading] = useState(false);
+
   async function fetchProducts() {
+    setLoading(true);
     const response = await axios.get<IProduct[]>(
       "https://fakestoreapi.com/products?limit=5"
     );
     setProducts(response.data);
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -18,6 +22,7 @@ function App() {
 
   return (
     <div className="container mx-auto max-w-2xl pt-5">
+      {loading && <p className="text-center">Loading...</p>}
       {products.map((product) => (
         <Product product={product} key={product.id} />
       ))}
